b89a869a72f1fd05e195194211f5dba709f5cf2c
Phil Moir <pgmoir@gmail.com>
2018-05-11 15:53:41 +0100

    ONly require a Compare log instance - dropped info, warning, etc.

--- log.go
+++ log.go
@@ -7,22 +7,16 @@ import (
 	"time"
 )
 
+// Compare is instance of the log
 var (
-	Trace   *log.Logger
-	Info    *log.Logger
-	Warning *log.Logger
-	Error   *log.Logger
 	Compare *log.Logger
 )
 
+// LogInit initialises the log file to detail comparison differences
 func LogInit(
-	traceHandle io.Writer,
-	infoHandle io.Writer,
-	warningHandle io.Writer,
-	errorHandle io.Writer,
 	compareHandle io.Writer) {
 
-	file, err := os.OpenFile("compare/changes_"+time.Now().Format("20060102150405")+".log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	file, err := os.OpenFile(LogName(), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalln("Failed to open log file", "changes.log", ":", err)
 	}
@@ -32,20 +26,9 @@ func LogInit(
 		log.Ldate|log.Ltime|log.Lshortfile)
 
 	Compare.SetFlags(0)
+}
 
-	Trace = log.New(traceHandle,
-		"TRACE: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-
-	Info = log.New(infoHandle,
-		"INFO: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-
-	Warning = log.New(warningHandle,
-		"WARNING: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-
-	Error = log.New(errorHandle,
-		"ERROR: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
+// LogName returns the folder and file name for creating a new log
+func LogName() string {
+	return "compare/changes_" + time.Now().Format("20060102150405") + ".log"
 }
